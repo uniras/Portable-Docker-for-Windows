e26bae31d3e6de64e355edfecea3abd9ee112ea4
Riafeed <tkappeng@gmail.com>
2020-08-10 07:23:56 +0000

    エラー処理

--- dockerconsole.bat
+++ dockerconsole.bat
@@ -2,6 +2,17 @@
 echo Linuxを起動しています。しばらくお待ちください...
 call %~dp0\start.bat
 call %~dp0\startwait.bat
+
+if errorlevel 0 goto start
+
+echo 接続に失敗しました。Qemuが起動していないようです。
+
+pause 
+
+exit
+
+:start
+
 call %~dp0\script\dockerconfig.bat
 
 cmd
--- startwait.bat
+++ startwait.bat
@@ -1,4 +1,7 @@
+@echo off
 call %~dp0\script\config.bat
 call %~dp0\script\downloadconfig.bat
 
 %~dp0\%TERATERM_PATH%\ttpmacro.exe /I %~dp0\script\startwait.ttl
+
+exit /b %errorlevel%

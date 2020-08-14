1fdfe748d8157a7900cd90eaa677cb274847a8e3
Riafeed <tkappeng@gmail.com>
2020-08-15 04:06:07 +0900

    修正

--- script/setup.bat
+++ script/setup.bat
@@ -1,7 +1,24 @@
 @echo off
-echo セットアップを開始します...
-
 call "%~dp0\config.bat"
+
+if not exist "%~dp0\..\%QEMU_HDD_PATH%" goto :setupok
+
+set /P ANSWER="すでにインストール済みのようです。実行すると仮想HDDのデータはすべて消去され、再度ファイルのダウンロードから始めますがよろしいですか (Y/N)？"
+
+if /i {%ANSWER%}=={y} (goto :formatok)
+if /i {%ANSWER%}=={yes} (goto :formatok)
+
+pause
+
+exit
+
+formatok:
+
+del /q "%~dp0\..\%QEMU_HDD_PATH%"
+
+setupok:
+
+echo セットアップを開始します...
 
 call "%~dp0\download.bat"
 

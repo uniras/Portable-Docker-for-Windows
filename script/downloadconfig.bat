05b85a2ae3f40622c32ef9833df5c4bc1996ac53
Riafeed <tkappeng@gmail.com>
2020-09-03 21:34:17 +0900

    Qemuバージョンアップ

--- script/downloadconfig.bat
+++ script/downloadconfig.bat
@@ -3,7 +3,7 @@
 :各種ソフトウェアのURL
 set WINGIT_URL="https://github.com/git-for-windows/git/releases/download/v2.28.0.windows.1/PortableGit-2.28.0-64-bit.7z.exe"
 set ALPINE_URL="http://dl-cdn.alpinelinux.org/alpine/v3.12/releases/x86_64/alpine-virt-3.12.0-x86_64.iso"
-set QEMU_URL="https://qemu.weilnetz.de/w64/2020/qemu-w64-setup-20200805.exe"
+set QEMU_URL="https://qemu.weilnetz.de/w64/2020/qemu-w64-setup-20200814.exe"
 set SEVENZ_URL="https://ja.osdn.net/frs/redir.php?m=jaist&f=sevenzip%%2F70662%%2F7z1900-x64.msi"
 set TERATERM_URL="https://ja.osdn.net/frs/redir.php?m=jaist&f=ttssh2%%2F72009%%2Fteraterm-4.105.zip"
 set DOCKERCLI_URL="https://github.com/StefanScherer/docker-cli-builder/releases/download/19.03.12/docker.exe"

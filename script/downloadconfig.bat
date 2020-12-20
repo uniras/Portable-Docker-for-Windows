da9e0cd0dba3ad06270e8f0dcf5db210f2e88807
RIAFeed <tkappeng@gmail.com>
2020-12-20 10:07:02 +0900

    URLを最新バージョンに

--- script/downloadconfig.bat
+++ script/downloadconfig.bat
@@ -1,15 +1,15 @@
 @echo off
 
 :各種ソフトウェアのURL
-set WINGIT_URL="https://github.com/git-for-windows/git/releases/download/v2.29.1.windows.1/PortableGit-2.29.1-64-bit.7z.exe"
-set ALPINE_URL="http://dl-cdn.alpinelinux.org/alpine/v3.12/releases/x86_64/alpine-virt-3.12.1-x86_64.iso"
-set QEMU_URL="https://qemu.weilnetz.de/w64/2020/qemu-w64-setup-20200814.exe"
+set WINGIT_URL="https://github.com/git-for-windows/git/releases/download/v2.29.2.windows.3/PortableGit-2.29.2.3-64-bit.7z.exe"
+set ALPINE_URL="https://dl-cdn.alpinelinux.org/alpine/v3.12/releases/x86_64/alpine-virt-3.12.3-x86_64.iso"
+set QEMU_URL="https://qemu.weilnetz.de/w64/2020/qemu-w64-setup-20201124.exe"
 set SEVENZ_URL="https://ja.osdn.net/frs/redir.php?m=jaist&f=sevenzip%%2F70662%%2F7z1900-x64.msi"
 set TERATERM_URL="https://ja.osdn.net/frs/redir.php?m=jaist&f=ttssh2%%2F72009%%2Fteraterm-4.105.zip"
-set DOCKERCLI_URL="https://github.com/StefanScherer/docker-cli-builder/releases/download/19.03.12/docker.exe"
+set DOCKERCLI_URL="https://github.com/StefanScherer/docker-cli-builder/releases/download/19.03.14/docker.exe"
 set DOCKERCMP_URL="https://github.com/docker/compose/releases/download/1.27.4/docker-compose-Windows-x86_64.exe"
 set DOCKER_EXPOSE_PLUGIN_URL="https://raw.githubusercontent.com/lukaszlach/clip/master/docker-clip"
-set WEBMIN_VERSION=1.960
+set WEBMIN_VERSION=1.962
 
 :各種ソフトウェアのパス
 set APP_DIR=app

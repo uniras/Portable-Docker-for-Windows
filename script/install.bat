d677e22ea4ce1cae2ca7c8b10da78b9f8e3ec449
RIAFeed <tkappeng@gmail.com>
2021-04-02 21:52:17 +0900

    Qemuの新バージョンの仕様に対応

--- script/install.bat
+++ script/install.bat
@@ -33,11 +33,11 @@ echo インストールを開始します。
 echo HDDイメージ作成...
 echo 最大%QEMU_HDD_DEF_SIZE%Bの設定でHDDイメージを作成します。
 
-"%~dp0\..\%QEMU_PATH%\qemu-img.exe" create -f qcow2 "%~dp0\..\%QEMU_HDD_PATH%" %QEMU_HDD_DEF_SIZE%
+"%~dp0\..\%QEMU_PATH%\qemu-img.exe" create -f qcow2 "%~dp0..\%QEMU_HDD_PATH%" %QEMU_HDD_DEF_SIZE%
 
 echo Qemu起動...
 
-start /min "Qemu" "%~dp0\..\%QEMU_EXE_PATH%" -L "%~dp0..\%QEMU_PATH%\" %QEMU_DISPLAY_MODE% %QEMU_SERIAL_OPT% %QEMU_CLOCK_OPT% -smp %QENU_USE_PROCESSOR% -boot d -m %QEMU_USE_MEMORY% -net nic,model=virtio -net %QEMU_NET_OPTION% -hda "%~dp0\..\%QEMU_HDD_PATH%" -cdrom "%~dp0\..\%QEMU_ISO_PATH%"
+start /min "Qemu" "%~dp0..\%QEMU_EXE_PATH%" -L "%~dp0..%QEMU_PATH%\" %QEMU_DISPLAY_MODE% %QEMU_SERIAL_OPT% %QEMU_CLOCK_OPT% -smp %QENU_USE_PROCESSOR% -boot d -m %QEMU_USE_MEMORY% -net nic,model=virtio -net %QEMU_NET_OPTION% -hda "%~dp0..\%QEMU_HDD_PATH%" -cdrom "%~dp0..\%QEMU_ISO_PATH%"
 
 echo インストールマクロ 起動...
 
@@ -45,7 +45,7 @@ echo インストールマクロ 起動...
 
 echo Qemu再起動...
 
-start /min "Qemu" "%~dp0\..\%QEMU_EXE_PATH%" -L "%~dp0..\%QEMU_PATH%\" %QEMU_DISPLAY_MODE% %QEMU_SERIAL_OPT% %QEMU_CLOCK_OPT% -smp %QENU_USE_PROCESSOR% -m %QEMU_USE_MEMORY% -net nic,model=virtio -net %QEMU_NET_OPTION% -hda "%~dp0\..\%QEMU_HDD_PATH%"
+start /min "Qemu" "%~dp0..\%QEMU_EXE_PATH%" -L "%~dp0..%QEMU_PATH%\" %QEMU_DISPLAY_MODE% %QEMU_SERIAL_OPT% %QEMU_CLOCK_OPT% -smp %QENU_USE_PROCESSOR% -m %QEMU_USE_MEMORY% -net nic,model=virtio -net %QEMU_NET_OPTION% -hda "%~dp0..\%QEMU_HDD_PATH%"
 
 echo セットアップマクロ 起動...
 
